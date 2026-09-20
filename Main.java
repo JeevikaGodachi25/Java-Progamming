b4a0aeca4d9a7319dca3c05f3d519d3f09b0643e
Jeevika Godachi <jeevikagodachi06@gmail.com>
2026-09-20 22:04:44 +0530

    Added a Java  data type   Program

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args)
+    {
+    int a=1234567;
+    long b=1234567891039049043L;
+    String name="Jeevika Godachi";
+    char c='$';
+    byte e =126;
+    short f =3277;
+    float z=234.54f;
+    double d=23.67893412;
+    boolean g=true;
+
+    System.out.println("Integer :"+a);
+    System.out.println("Long  :"+b);
+    System.out.println("byte :"+e);
+    System.out.println("Short :"+f);
+    System.out.println("String :"+name);
+    System.out.println("Float :"+z);
+    System.out.println("Double :"+d);
+    System.out.println("Char :"+c);
+    System.out.println("Boolean :"+g);
+    }
+
+}
