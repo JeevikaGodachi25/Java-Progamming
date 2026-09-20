97059d9d33b3b03457a758cacafdb6bc4f03b789
Jeevika Godachi <jeevikagodachi06@gmail.com>
2026-09-20 22:13:57 +0530

    Added a Swapping of data program in Java.

---
+++ SwapData.java
@@ -0,0 +1,14 @@
+public class SwapData {
+    public static void main(String[] args) {
+        int Jeevika=100;
+        int Gowri=200;
+            System.out.println("Jeevika: "+Jeevika);
+            System.out.println("Gowri :"+Gowri);
+        int temp=Jeevika;
+        Jeevika=Gowri;
+        Gowri=temp;
+        System.out.println("After swapping:");
+        System.out.println("Jeevika: "+Jeevika);
+        System.out.println("Gowri :"+Gowri);
+    }
+}
